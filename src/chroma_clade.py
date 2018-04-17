9f8a8821e8de8b756070b947f990245d55a04b2a
Chris Monit <c.monit.12@ucl.ac.uk>
2018-04-17 15:01:05 +0100

    Found that the previous version too specialised to figtree example. Here I've separated the colour annotation and output steps, so we first add colour labels to clade.color field and can then interpret this according to the output format we want (ie figtree or xml).
    This version therefore has same functionality as before but
    implemented in a more extendible way.

--- src/chroma_clade.py
+++ src/chroma_clade.py
@@ -1,19 +1,21 @@
+#!/usr/bin/python
 from Bio import Phylo
 from Bio import AlignIO, SeqIO
 from itertools import chain 
 
 from Bio.Nexus import Nexus 
 from Bio.Phylo import Newick, NewickIO 
+from Bio.Phylo.BaseTree import BranchColor
 import copy
 
 AA_STATES = ["A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "Y", "*", "-", "X"]
 N_STATES = len(AA_STATES)
 
-COLOURS = { 'A':'#FF0000', 'C':'#009933', 'D':'#990000', 'E':'#FF0066', 'F':'#6666FF', 'G':'#00CC33', 'H':'#FFCC00', 'I':'#660066', 'K':'#CC3300', 'L':'#00CCFF', 'M':'#FF9900', 'N':'#FF9966', 'P':'#CC0099', 'Q':'#FF00CC', 'R':'#990000', 'S':'#336600', 'T':'#FF6699', 'V':'#FF66FF', 'W':'#0000FF', 'Y':'#0099FF', '*':'#000000', '-':'#000000', 'X':'#000000' }
+BLANK_BRANCH_COL = '#797D7F' # dark grey
+COLOURS = { 'A':'#FF0000', 'C':'#009933', 'D':'#990000', 'E':'#FF0066', 'F':'#6666FF', 'G':'#00CC33', 'H':'#FFCC00', 'I':'#660066', 'K':'#CC3300', 'L':'#00CCFF', 'M':'#FF9900', 'N':'#FF9966', 'P':'#CC0099', 'Q':'#FF00CC', 'R':'#990000', 'S':'#336600', 'T':'#FF6699', 'V':'#FF66FF', 'W':'#0000FF', 'Y':'#0099FF', '*':BLANK_BRANCH_COL, '-':BLANK_BRANCH_COL, 'X':BLANK_BRANCH_COL }
 
 COL_ATTRIB = "[&!color=%s]"
 STATE_SUFFIX = "__site_%d__%s"
-BLANK_BRANCH_COL = '000000'
 
 # Structure of a Nexus tree-only file 
 NEX_TEMPLATE = """#NEXUS 
@@ -27,18 +29,13 @@ End;"""
 # 'index' starts from 1; 'tree' is the Newick tree string 
 TREE_TEMPLATE = "Tree tree%(index)d=%(tree)s" 
 
-def colour_taxon(name, value_dict=COLOURS, n_chars=1, annotation_string=COL_ATTRIB):
-    return name + annotation_string % value_dict[name[-n_chars]]
-
-def identity(name): # use for no annotation
-    return name
-
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument( "tree" )
     parser.add_argument( "alignment")
     parser.add_argument( "-b", action="store_true", help="Colour branches in addition to tip names")
+    parser.add_argument( "-xml", action="store_true", help="Save as PhyloXML format, e.g. compatible with Archeopteryx")
     parser.add_argument( "-tf", default="newick", help="Tree file format, 'newick' (default), 'nexus' or 'phyloxml'" )
     parser.add_argument( "-af", default="fasta", help="Alignment file format, 'fasta' (default)" ) # TODO give more info
     args = parser.parse_args()
@@ -58,24 +55,36 @@ def main():
     taxon_dict = dict([ (aln[i].id, i) for i in range(len(aln)) ]) # maps taxon identifiers to their alignment indices 
     
     trees = []
-    
+    colour_type = "native" if args.xml else "figtree"
     for iSite in range(len(aln)):
         tree_copy = copy.deepcopy(tree)
-        if args.b:
-            colour_tree(tree_copy.root, aln, taxon_dict, iSite)
-        else:
-            colour_tips_only(tree_copy, aln, taxon_dict, iSite)
+        colour_tree(tree_copy.root, aln, taxon_dict, iSite)
+        annotate_tips_only(tree_copy, aln, taxon_dict, iSite) # add site and state info
         trees.append(tree_copy)
     
-    #Phylo.write(trees[0], "xml.out.tre", "phyloxml")
+#    if args.xml:
+#        Phylo.write(trees, "xml.out.tre", "phyloxml")
+#    else:
+    output_figtree(trees, args.b)
+    
+def output_figtree(coloured_trees, colour_branches):
 
-    tip_annotation_func = (identity if args.b else colour_taxon)
-    print nexus_text(trees, tip_annotation_func).replace("'", "") # the Bio code automatically adds inverted commas to colour attribute lables, which prevents figtree reading them as annotations
+    if colour_branches:
+        for tree in coloured_trees:
+            for clade in tree.get_nonterminals():
+                clade.name = COL_ATTRIB % BranchColor.to_hex(clade.color) # colour is stored as RGB vector, but we want RGB hex
+            for clade in tree.get_terminals():
+                clade.name += COL_ATTRIB % BranchColor.to_hex(clade.color)
+        tip_annotation_func = identity
+    else:
+        tip_annotation_func = colour_taxon
+
+    print nexus_text(trees, tip_annotation_func).replace("'", "")
+    # the Bio code automatically adds inverted commas to colour attribute lables, which prevents figtree reading them as annotations
 
 
-
-def colour_tips_only(tree, alignment, taxon_dict, site, states=AA_STATES):
-    """ Apply colour labels to tips only """
+def annotate_tips_only(tree, alignment, taxon_dict, site, states=AA_STATES):
+    """ Apply labels to tips showing site and state information (not colour)"""
     for tip in tree.get_terminals():
         state = alignment[ taxon_dict[tip.name] ][site].upper()
         tip.name += (STATE_SUFFIX % (site+1, state))
@@ -90,9 +99,9 @@ def colour_tree(parent, alignment, taxon_dict, site, states=AA_STATES):
     """
     if parent.is_terminal():
         state = alignment[ taxon_dict[parent.name] ][site].upper()
+        parent.color = COLOURS[state] # stored as RGB tuple
         parent_vector = [0] * N_STATES 
         parent_vector[ AA_STATES.index(state) ] = 1
-        parent.name += (STATE_SUFFIX % (site+1, state)) + (COL_ATTRIB % COLOURS[state])
         return parent_vector
     else:
         parent_vector = [1] * N_STATES
@@ -107,10 +116,16 @@ def colour_tree(parent, alignment, taxon_dict, site, states=AA_STATES):
             col = COLOURS[ AA_STATES[parent_vector.index(1)] ]
         else:
             raise ValueError("Incorrect parent vector!")
-        parent.name = COL_ATTRIB % col
+        parent.color = col # stored as RGB tuple
         return parent_vector
 
 # io
+def colour_taxon(name, value_dict=COLOURS, n_chars=1, annotation_string=COL_ATTRIB):
+    return name + annotation_string % value_dict[name[-n_chars]]
+
+def identity(name): # use for no annotation
+    return name
+
 
 # TODO could include rooted/unrooted tree information, as is now standard in nexus format
 def nexus_text(obj, tip_annotation_func, **kwargs):
