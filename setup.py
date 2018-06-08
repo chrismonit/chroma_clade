9a8f57fe11ee2bf3d99402f36ad4c0a1c2547e01
Chris Monit <c.monit.12@ucl.ac.uk>
2018-06-08 17:55:38 +0100

    added some bits to setup.py in preparation for py2exe compatibility, and made an image file for icon on mac

--- setup.py
+++ setup.py
@@ -8,14 +8,21 @@ from setuptools import setup
 _VERSION="1.0"
 
 APP = ['src/gui.py']
-DATA_FILES = []
-OPTIONS = {'resources': 'src/default_colour.csv,src/title.gif,src/tree.gif,src/col.tree.gif'}
+DATA_FILES = [] # may want to use this for py2exe
+PY2APP_OPTIONS = {'resources': 
+                    'src/default_colour.csv,src/title.gif,src/tree.gif,src/col.tree.gif',
+                    'iconfile': 'src/tree.icns'
+                    }
+PY2EXE_OPTIONS = {"packages": ["biopython"]}
 
 setup(
     app=APP,
     data_files=DATA_FILES,
-    options={'py2app': OPTIONS},
+    options={'py2app': PY2APP_OPTIONS},
     setup_requires=['py2app'], # will this mess up py2exe?
+
+    windows=['gui.py'],
+    console=['chroma_clade.py'],
 
     description = "A GUI and CLI app for producing phylogenetic tree files coloured by observed molecular sequence states",
     author = 'Christopher Monit', 
@@ -23,4 +30,5 @@ setup(
     url = 'https://github.com/chrismonit/chroma_clade',
     install_requires=['biopython'],
     long_description=open("README.txt").read(),
+
 )
