baa8e0c4b3a255cce02a922f7273d4e89ce7d8cf
Chris Monit <c.monit.12@ucl.ac.uk>
2019-08-19 13:25:46 +0100

    Completed switch to pyintaller system for producing macOS bundle. Deleted pyapp output directory in apps/mac renamed apps/mac_pyinstaller as apps/mac deleted src/setup.py changed file containing mac pyinstaller command to a runnable script changed name for bundle name to ChromaClade (from chroma_clade) NB the pyinstaller spec file follows convention appname.spec Therefore ChromaClade.spec refers to mac output while chroma_clade.spec will be for windows output. Consequently the mac bundle and windows exe will have different names too

--- src/mac_pyinstaller.sh
+++ src/mac_pyinstaller.sh
@@ -4,15 +4,16 @@ pyinstaller \
 --add-data tree.png:. \
 --add-data col.tree.png:. \
 --add-data default_colour.csv:. \
---distpath ../apps/mac_pyinstaller \
---workpath ../apps/mac_pyinstaller/build \
---name chroma_clade_pyinst \
+--distpath ../apps/mac \
+--workpath ../apps/mac/build \
+--name ChromaClade \
 --windowed \
 --onefile \
+--clean \
 --exclude altgraph --exclude certifi --exclude chardet --exclude cycler --exclude DendroPy --exclude idna --exclude kiwisolver --exclude macholib --exclude matplotlib --exclude numpy --exclude pandas --exclude patsy --exclude pyparsing --exclude PyPDF2 --exclude python-dateutil --exclude pytz --exclude requests --exclude scikit-learn --exclude scipy --exclude setuptools --exclude six --exclude sklearn --exclude statsmodels --exclude tkcolorpicker --exclude urllib3 \
 gui.py
 
 # apparently PyInstaller 3.5 has a bug relating to tcl/tk on macOS
-compensated for by copying in files manually
+# compensated for by copying in files manually
 # see https://github.com/pyinstaller/pyinstaller/issues/3820
-cp -r ../apps/mac_pyinstaller/lib ../apps/mac_pyinstaller/chroma_clade_pyinst.app/Contents
+cp -r ../apps/mac/lib ../apps/mac/ChromaClade.app/Contents
